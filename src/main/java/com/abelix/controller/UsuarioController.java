26a81905e79af33e5018f235b46bc9b5d2f80947
Natallegarcia <natallegarcia2015@gmail.com>
2025-06-15 13:26:29 -0300

    WebService de login que faz encriptação de senha com BCrypt com métodos de cadastrar novo usuário e senha e validar a senha comparando com o BD

--- src/main/java/com/abelix/controller/UsuarioController.java
+++ src/main/java/com/abelix/controller/UsuarioController.java
@@ -1,12 +1,16 @@
 package com.abelix.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,9 +22,12 @@ import com.abelix.model.UsuarioModel;
 public class UsuarioController {
 
     private final UsuarioRepository repository;
+    private final PasswordEncoder encoder; 
 
-    public UsuarioController(UsuarioRepository repository) {
+    public UsuarioController(UsuarioRepository repository, PasswordEncoder encoder) {
         this.repository = repository;
+        this.encoder = encoder;
+		
     }
 
     @GetMapping("/listarTodos")
@@ -32,6 +39,36 @@ public class UsuarioController {
     @PostMapping("/salvar")
     public ResponseEntity<UsuarioModel> salvar(@RequestBody UsuarioModel usuario) {
     	 System.out.println("Recebido: " + usuario);
+    	 usuario.setPassword(encoder.encode(usuario.getPassword()));
         return ResponseEntity.ok(repository.save(usuario));
+        
     }
+    
+    @GetMapping("/validarSenha")
+    public ResponseEntity<Boolean> validarSenha(@RequestParam String login, @RequestParam String password){
+    	
+    	
+    	Optional<UsuarioModel> optUsuario = repository.findByLogin(login); // 1 consulto o usuário. Se o usuario não for encontrado pelo login será retornado não autorizado com o valor falso
+    	if (optUsuario.isEmpty()) {
+    		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+    	}
+    	
+    	boolean valid = false; 
+    	
+    	UsuarioModel usuario = optUsuario.get();
+    	valid = encoder.matches(password,optUsuario.get().getPassword()); // valida se a senha informada é a mesma senha do banco de dados 
+    	// o método matches está comparando a senha aberta com a senha encriptada feito pelo encoder. 
+    	HttpStatus status;
+		if (valid)
+			status = HttpStatus.OK;
+		else
+			status = HttpStatus.UNAUTHORIZED;
+    	
+    	if (!valid) {}
+    	
+    	return ResponseEntity.status(status).body(valid);  //retornar o usuário pelo login
+    	
+    }
+    
+    
 }
